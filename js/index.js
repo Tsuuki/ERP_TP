3c998148a9016f9b5e50b057d066481cd1945ca6
Azzouz <azzouzminfo@yahoo.fr>
2018-11-06 13:30:21 +0100

    Merge index.js and js/index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const { BrowserWindow } = require ('electron').remote;
 const path = require('path');
 
-
 const projectBtn = document.getElementById("manageProjects");
 
 projectBtn.addEventListener('click', (event) => {
@@ -22,4 +21,26 @@ projectBtn.addEventListener('click', (event) => {
     win.on('closed', () => {
         win = null;
     })
-})
+});
+
+var listDom = $("#listDataEmployees");
+for(var i=0;i<employees.length;i++) {
+    listDom.append("<div>");
+    listDom.append("<span>" + employees[i].name + "</span>");
+    listDom.append("<span>" + employees[i].firstname + "</span>");
+    listDom.append("<span>" + employees[i].jobType + "</span>");
+    listDom.append("<span>" + employees[i].efficiency + "</span>");
+    listDom.append("<span>" + employees[i].hiringDate + "</span>");
+    listDom.append("</div>");
+}
+
+var listDom = $("#listDataProjects");
+for(var i=0;i<projects.length;i++) {
+    listDom.append("<div>");
+    listDom.append("<span>" + projects[i].client + "</span>");
+    listDom.append("<span>" + projects[i].dateStart + "</span>");
+    listDom.append("<span>" + projects[i].remainDevDays + "</span>");
+    listDom.append("<span>" + projects[i].remainProjDays + "</span>");
+    listDom.append("<span>" + projects[i].shipment + "</span>");
+    listDom.append("</div>");
+}
