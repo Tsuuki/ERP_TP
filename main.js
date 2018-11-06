55d52e3bd025b601fa27c698df7c178e090ca01d
Jordan Hiertz <hiertzjordan@gmail.com>
2018-11-06 09:11:42 +0100

    reference lock package

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 const { app, BrowserWindow } = require ('electron');
 
+let mainWindow;
+
 function createWindow () {
 
     // Créer le browser window
-    win = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
     // Charge le fichier de l'application
-    win.loadFile('index.html');
+    mainWindow.loadFile('index.html');
 
     // npm run debug
     if(process.argv[2] && process.argv[2] === "debug") {
-        win.webContents.openDevTools();
+        mainWindow.webContents.openDevTools();
     }
-
 }
 
 app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    // Sur macOS, il est commun pour une application et leur barre de menu
+    // de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // Sur macOS, il est commun de re-créer une fenêtre de l'application quand
+    // l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
+    if (win === null) {
+        createWindow()
+    }
+})
